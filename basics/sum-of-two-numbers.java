81cdb0a4eea149f59914cba0d51cc6a5a5f9743b
ashay-maheshwari <ashay@vlabs.ac.in>
2017-01-18 04:40:19 +0530

    add basic programs

---
+++ basics/sum-of-two-numbers.java
@@ -0,0 +1,24 @@
+/*This is first program in java 
+To display sum of two numbers 
+Execute -
+javac <filename.java>
+java <CLassname>
+*/
+
+
+import java.lang.*;
+class Sum
+{
+	public static void main(String ar[])
+	{
+		int x,y;
+		x = 5;
+ 		y =7;
+		int sum = x + y;
+     		System.out.print("sum of numbers = " + sum); //returns 10. print function braces from left to right
+	}
+
+}
+
+
+
---
+++ basics/welcome-to-java.java
@@ -0,0 +1,21 @@
+/*This is first program in java 
+To display a welcome message on terminal 
+Execute -
+javac <filename.java>
+java <CLassname>
+*/
+
+
+import java.lang.*;
+class WelcomeToJava
+{
+	public static void main(String ar[])
+	{
+	 System.out.print("welcome to java"); //Sytem is class and out is a static variable in class System. Internally
+        // it creates an object of Printstream Class where print methods belong to 
+	}
+
+}
+
+
+
