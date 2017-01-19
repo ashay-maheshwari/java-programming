0aae390f122eda46fa516407b7f0e07b17e402ac
ashay-maheshwari <ashay@vlabs.ac.in>
2017-01-19 17:11:19 +0530

    add simple artiehmatic java code

---
+++ basics/SimpleArithemetic.java
@@ -0,0 +1,18 @@
+class SimpleArithematic 
+{
+	public static void main(String ar[]) {
+		short x = 6;
+		int y = 4;
+		float a = 13.5f;
+		float b = 6f;
+
+		System.out.println("x is " + x + ", y is " + y);
+		System.out.println("x + y = " + (x + y));
+		System.out.println("x - y = " + (x - y));
+		System.out.println("x / y = " + (x / y));
+		System.out.println("x % y = " + (x % y));
+	} //end main 
+} // end class
+		
+	
+	
